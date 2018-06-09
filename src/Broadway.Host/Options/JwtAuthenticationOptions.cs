0e313f06b876b26bba3e6e2a3a45de7dfef03b8e
denisivan0v <denis@ivanovdenis.ru>
2018-06-09 15:22:40 +0700

    AIM set as authentication authority

--- src/Broadway.Host/Options/JwtAuthenticationOptions.cs
+++ src/Broadway.Host/Options/JwtAuthenticationOptions.cs
@@ -2,8 +2,7 @@
 {
     public class JwtAuthenticationOptions
     {
+        public string Authority { get; set; }
         public string Audience { get; set; }
-        public string Issuer { get; set; }
-        public string Certificate { get; set; }
     }
 }

b8a148d1fc23faea6c824d2f278b119d65ed1c3e
denisivan0v <denis@ivanovdenis.ru>
2018-06-09 16:16:36 +0700

    db schema generation script fixed

--- generate-projections-db-schema.sh
+++ generate-projections-db-schema.sh
@@ -4,5 +4,5 @@ dotnet ef migrations add -p src/Broadway -s src/Broadway.Silo InitialCreate;
 dotnet ef migrations script -p src/Broadway -s src/Broadway.Silo -o projections-db-schema.sql;
 rm -rf src/Broadway/Migrations;
 sed -i '' -e '/^\s*$/d' projections-db-schema.sql;
-tail -n +6 projections-db-schema.sql | sed -e '$d' -e '$d' > tmp;
+tail -n +6 projections-db-schema.sql | sed -e '$d' | sed -e '$d' > tmp;
 mv tmp projections-db-schema.sql;
